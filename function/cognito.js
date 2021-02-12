babc62fc7917da8bbcbc6d1bea9fae689bc60f0f
dhsimpson <qeadzc9503@naver.com>
2021-02-13 00:06:00 +0900

    admin, 일반 회원 나눠 관리하기. TODO: 중복되는 이름 겹치지 말도록

--- function/cognito.js
+++ function/cognito.js
@@ -3,53 +3,77 @@ require('dotenv').config();
 var AmazonCognitoIdentity = require('amazon-cognito-identity-js-node');
 var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 var CognitoUserAttribute = AmazonCognitoIdentity.CognitoUserAttribute;
-// 이 두 개는 환경 변수에 넣기.
-const UserPoolId = process.env.UserPoolId;
-const ClientId = process.env.ClientId;
-const poolData = {
-    UserPoolId,
-    ClientId
+// Admin 계정의 경우
+const adminUserPoolId = process.env.adminUserPoolId;
+const adminClientId = process.env.adminClientId;
+const adminPoolData = {
+    UserPoolId: adminUserPoolId,
+    ClientId: adminClientId
 }
-const userPool = new CognitoUserPool(poolData);
+const adminUserPool = new CognitoUserPool(adminPoolData);
+
+// Normal 계정의 경우
+const normalUserPoolId = process.env.normalUserPoolId;
+const normalClientId = process.env.normalClientId;
+const normalPoolData = {
+    UserPoolId: normalUserPoolId,
+    ClientId: normalClientId
+}
+const normalUserPool = new CognitoUserPool(normalPoolData);
 
 // 로그인
-module.exports.login = async function login (username, password, res) {
+module.exports.login = async function login(username, password, userLevel, res) {
+    let userPool;
+    if (userLevel === "admin") { userPool = adminUserPool; }
+    else if (userLevel === "normal") { userPool = normalUserPool; }
     const userData = {
         Username: username,
         Pool: userPool
     }
     const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-    
+
     const authenticationData = {
         Username: username,
         Password: password,
     };
     const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-        try{
-            await cognitoUser.authenticateUser(authenticationDetails, {
-                onSuccess: async function (result) {
-                    res.send(result);
-                    // var accessToken = result.getAccessToken().getJwtToken();
-                },
-                onFailure: async function (err) {
-                    res.send(err);
-                }
-            });
-        }catch(err) {
-            res.send({success:failure, error: err});
-        }
+    try {
+        await cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: async function (result) {
+
+                await cognitoUser.getUserAttributes((err,data) => {
+                    if(err){ res.send(err); } 
+                    else if(data){
+                        result.nickname = data[2].Name.Value;
+                        res.send(result);
+                    }});
+
+                // var accessToken = result.getAccessToken().getJwtToken();
+            },
+            onFailure: async function (err) {
+                res.send(err);
+            }
+        });
+    } catch (err) {
+        res.send({ success: failure, error: err });
+    }
 }
 
 // 회원 가입
-module.exports.singup = async function singup (username, password, email, res) {
+module.exports.singup = async function singup(username, password, email, nickname, userLevel, res) {
+    let userPool;
+    if (userLevel === "admin") { userPool = adminUserPool; }
+    else if (userLevel === "normal") { userPool = normalUserPool; }
     const userData = {
         Username: username,
         Pool: userPool
     }
     const attributeList = [];
     const attributeEmail = new CognitoUserAttribute("email", email);
+    const attributeNickname = new CognitoUserAttribute("nickname", nickname);
     attributeList.push(attributeEmail);
-    try{
+    attributeList.push(attributeNickname);
+    try {
         await userPool.signUp(username, password, attributeList, null, function (err, result) {
             if (err) {
                 res.send(err);
@@ -58,20 +82,23 @@ module.exports.singup = async function singup (username, password, email, res) {
                 res.send(resultCognitoUser.getUsername());
             }
         });
-    }catch (err){
-        res.send({success: "failure", error: err});
+    } catch (err) {
+        res.send({ success: "failure", error: err });
     }
 }
 
 
 // 회원 가입 이메일 인증 코드
-module.exports.confirm = async function confirm (username, confirmCode, res) {
+module.exports.confirm = async function confirm(username, confirmCode, userLevel, res) {
+    let userPool;
+    if (userLevel === "admin") { userPool = adminUserPool; }
+    else if (userLevel === "normal") { userPool = normalUserPool; }
     const userData = {
         Username: username,
         Pool: userPool
     }
     const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData)
-    try{
+    try {
         await cognitoUser.confirmRegistration(confirmCode, true, function (err, result) {
             if (err) {
                 res.send(err);
@@ -79,19 +106,22 @@ module.exports.confirm = async function confirm (username, confirmCode, res) {
                 res.send(result);
             }
         })
-    }catch(err) {
+    } catch (err) {
         res.send(err);
     }
 }
 
 // 회원 가입 인증 코드 재전송
-module.exports.resendCode = async function resendCode (username, res) {
+module.exports.resendCode = async function resendCode(username, userLevel, res) {
+    let userPool;
+    if (userLevel === "admin") { userPool = adminUserPool; }
+    else if (userLevel === "normal") { userPool = normalUserPool; }
     const userData = {
         Username: username,
         Pool: userPool
     }
     const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData)
-    try{
+    try {
         await cognitoUser.resendConfirmationCode(function (err, result) {
             if (err) {
                 res.send(err);
@@ -99,7 +129,23 @@ module.exports.resendCode = async function resendCode (username, res) {
                 res.send(result);
             }
         })
-    }catch(err) {
+    } catch (err) {
         res.send(err);
     }
 }
+
+let userPool = normalUserPool;
+
+const userData = {
+    Username: "dongdong",
+    Pool: userPool
+}
+const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+// const getAtt = async() => {await cognitoUser.getUserAttributes((err,data) => {
+// if(err) console.log("에러놈 :"+ err);
+// if(data) console.log(data);
+// } )
+// }
+
+// getAtt();
--- route/user.js
+++ route/user.js
@@ -4,21 +4,21 @@ const router = express.Router();
 const cognito = require('../function/cognito');
 
 // 회원 관련
-router.post('/login', async (req,res) => {
-    const {username, password} = req.body;
-    await cognito.login(username, password, res);
+router.post('/login', async (req, res) => {
+    const { username, password, userLevel } = req.body;
+    await cognito.login(username, password, userLevel, res);
 });
-router.post('/signup', async (req,res) => { // c.f. 닉네임도 할지?
-    const {username, password, email} = req.body;
-    await cognito.singup(username, password, email, res);
+router.post('/signup', async (req, res) => {
+    const { username, password, email, nickname, userLevel } = req.body;
+    await cognito.singup(username, password, email,nickname, userLevel, res);
 });
-router.post('/confirm', async (req,res) => {
-    const {username, confirmCode} = req.body;
-    await cognito.confirm(username, confirmCode, res);
+router.post('/confirm', async (req, res) => {
+    const { username, confirmCode, userLevel } = req.body;
+    await cognito.confirm(username, confirmCode, userLevel, res);
 })
-router.post('/resendCode', async (req,res) => {
-    const { username } = req.body;
-    await cognito.resendCode(username, res);
+router.post('/resendCode', async (req, res) => {
+    const { username, userLevel } = req.body;
+    await cognito.resendCode(username, userLevel, res);
 })
 
 //TODO : 토큰 만료 시 새로 토큰 가져오기
