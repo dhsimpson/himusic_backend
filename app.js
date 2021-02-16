4da7d470d07f5605f06d403560ee9c156b918962
dhsimpson <qeadzc9503@naver.com>
2021-02-17 02:04:45 +0900

    얘들은 언제 수정했었는지 모름 ㅠㅠ

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const cors = require('cors');
 const app = express();
 const port = 8080;
 const router_user = require('./route/user');
+const router_board = require('./route/board');
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use('/user', router_user);
+app.use('/board', router_board);
 
 app.listen(port, () => {
     console.log('Broker API is listening on port'+port);
--- route/board.js
+++ route/board.js
@@ -1,18 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
 const board = require('../function/board');
+const s3 = require('../function/s3');
+const fs = require('fs');
 
-// 회원 관련
-router.post('/create', async (req,res) => {
-    const { tableName, author, title, content } = req.body;
-    await board.createBoard(tableName, author, title, content, res);
+// 파일
+//s3.upload.single('file')
+router.post('/upload', s3.upload.fields([{name:'content'},{name:'file'},{name:'video'}]), async(req,res)=>{
+    console.log(req.files);
+    res.send({
+        success:1,
+        keys: req.files // front에서 다시 한 번 서버에 전달하고 서버가 dynamo에 전달
+    });
+    // s3.upload(req, res, function(err) {
+    //     if (err instanceof multer.MulterError) {
+    //       return next(err);
+    //     } else if (err) {
+    //       return next(err);
+    //     }
+    //     // console.log('원본파일명 : ' + req.file.originalname)
+    //     // console.log('저장파일명 : ' + req.file.filename)
+    //     // console.log('크기 : ' + req.file.size)
+    //     // console.log('경로 : ' + req.file.location) 
+    //     return res.json({success:1});
+    //   });
+})
+
+// 게시글 한 개 읽어오기
+router.get('/readPost', async(req,res) => {
+    await board.readPost(req.query, res);
+} );
+// 데이터 (content, file, video)
+router.get('/readData', async(req,res) => {
+    await board.readData(req.query, res);
+} );
+
+// 게시판
+router.post('/post', async (req,res) => {
+    // const { tableName, author, title, keys, month, date } = req.body;
+    await board.postBoard(req.body, res);
 });
 router.get('/read', async (req,res) => { // c.f. 닉네임도 할지?
     const { tableName, startIndex, queryAmount } = req.body;
     await board.readBoard(tableName, startIndex, queryAmount, res);
 });
-router.update('/update', async (req,res) => {
+router.put('/update', async (req,res) => {
     const { tableName, author, updateIndex, title, content } = req.body;
     await board.updateBoard(tableName, author, updateIndex, title, content, res);
 })
