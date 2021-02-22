6e4b7d9a894d619bc499a6b399f661514ec9df20
dhsimpson <qeadzc9503@naver.com>
2021-02-23 00:08:35 +0900

    app.js에 cors 관련 use 추가

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ const router_post = require('./route/post');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+    res.header("Access-Control-Allow-Credentials", true);
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(204);
+    }
+    next();
+  });
 app.use('/user', router_user);
 app.use('/board', router_board);
 app.use('/post', router_post); // post 는 게시글 임. 게시글 + 게시글에 포함 될 파일들을 전부 포함.
